182d907d8467937a0da72c7de09eb95ec77bf2a9
Mayank Talwar <dcmtalwar@gmail.com>
2020-10-24 17:53:16 +0530

    pickle optimal LR

--- Linear-Regression/main.py
+++ Linear-Regression/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import pickle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
 
 data = pd.read_csv("Linear-Regression/student-dataset/student-mat.csv", sep = ";") #load dataset
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]] #trim dataset to selected attributes
@@ -12,14 +15,28 @@ predict = "G3" #Set the label(the thing to predict using attributes)
 x = np.array(data.drop([predict], 1)) #making an array of the attributes (exluding the label)
 y = np.array(data[predict]) #making an array of the label's real values
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
-#splitting x and y to use one portion for training and the other for testing (in this case 10%)
-#this is needed so that the program hasn't already seen the data it is trying to predict after training
+i = 0
+best = 0
+while True:
+    i += 1
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+    #splitting x and y to use one portion for training and the other for testing (in this case 10%)
+    #this is needed so that the program hasn't already seen the data it is trying to predict after training
 
-linear = linear_model.LinearRegression() #free model for LR, thanks sklearn
-linear.fit(x_train, y_train) #plots the best fit line in multidimensional space using the values given
-acc = linear.score(x_test, y_test) #uses the best fit line to determine the label and returns the accuracy
-print(f'Accuracy: {acc*100}%')
+    linear = linear_model.LinearRegression() #free model for LR, thanks sklearn
+    linear.fit(x_train, y_train) #plots the best fit line in multidimensional space using the values given
+    acc = linear.score(x_test, y_test) #uses the best fit line to determine the label and returns the accuracy
+    if acc > best:
+        print(f'Accuracy: {acc*100}%')
+        best = acc
+    if acc > 0.98:
+        with open("stud_model.pickle", "wb") as f:
+            pickle.dump(linear, f)
+        break
+print(i)
+inp = open("stud_model.pickle", "rb")
+linear = pickle.load(inp)
+
 print(f'Coefficients: {linear.coef_}') #.coef_ returns a list of the coefficients that make the best fit line
 print(f'Intercept: {linear.intercept_}') #.intercept_ returns the intercept used in the best fit line
 
