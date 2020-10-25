c70cdb46d8174c1ad66972ec6870d6650b29cd8f
Mayank Talwar <dcmtalwar@gmail.com>
2020-10-25 21:11:57 +0530

    matplotlib

--- Linear-Regression/main.py
+++ Linear-Regression/main.py
@@ -26,29 +26,30 @@ predict = "G3" #Set the label(the thing to predict using attributes)
 x = np.array(data.drop([predict], 1)) #making an array of the attributes (exluding the label)
 y = np.array(data[predict]) #making an array of the label's real values
 
-i = 0
-best = 0
-while True:
-    i += 1
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
-    #splitting x and y to use one portion for training and the other for testing (in this case 10%)
-    #this is needed so that the program hasn't already seen the data it is trying to predict after training
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+# i = 0
+# best = 0
+# while True:
+#     i += 1
+#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+#     #splitting x and y to use one portion for training and the other for testing (in this case 10%)
+#     #this is needed so that the program hasn't already seen the data it is trying to predict after training
 
-    linear = linear_model.LinearRegression() #free model for LR, thanks sklearn
-    linear.fit(x_train, y_train) #plots the best fit line in multidimensional space using the values given
-    acc = linear.score(x_test, y_test) #uses the best fit line to determine the label and returns the accuracy
-    if i % 100000 == 0:
-        print(f'Just crossed {i}')
-    if acc > best:
-        print(f'Accuracy: {acc*100}%')
-        best = acc
-        with open("stud_model.pickle", "wb") as f: #saving the model
-            pickle.dump(linear, f)
-        if acc > 0.99:
-            break
-    elif acc > 0.98:
-        print("Another one!", i)
-print(i)
+#     linear = linear_model.LinearRegression() #free model for LR, thanks sklearn
+#     linear.fit(x_train, y_train) #plots the best fit line in multidimensional space using the values given
+#     acc = linear.score(x_test, y_test) #uses the best fit line to determine the label and returns the accuracy
+#     if i % 100000 == 0:
+#         print(f'Just crossed {i}')
+#     if acc > best:
+#         print(f'Accuracy: {acc*100}%')
+#         best = acc
+#         with open("stud_model.pickle", "wb") as f: #saving the model
+#             pickle.dump(linear, f)
+#         if acc > 0.99:
+#             break
+#     elif acc > 0.98:
+#         print("Another one!", i)
+# print(i)
 inp = open("stud_model.pickle", "rb")
 linear = pickle.load(inp) #load the model
 
@@ -58,3 +59,10 @@ print(f'Intercept: {linear.intercept_}') #.intercept_ returns the intercept used
 predictions = linear.predict(x_test) #returns a list of all the predictions made by the model for given data
 for i in range(y_test.size):
     print(f'Data: {x_test[i]}, Prediction: {predictions[i]}, Actual score: {y_test[i]}')
+
+p = "G2"
+style.use("ggplot") #makes the graph look good lol
+pyplot.scatter(data[p], data[predict]) #plots stuff (x, y) making a scatter plot graph
+pyplot.xlabel(p) #names the x-axis
+pyplot.ylabel("Final Grade") #names the y-axis
+pyplot.show() #shows the graph in a window
